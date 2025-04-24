bc8c89df8cbba4a5c2ab1893c490e39e466563b7
ymiguell <miguelmelo.o.fut@gmail.com>
2025-04-23 21:07:35 -0300

    exercicise java

---
+++ tipo-variaveis/src/Operadores.java
@@ -0,0 +1,19 @@
+public class Operadores {
+    public static void main(String[] args) {
+       
+        boolean condicao1 = true;
+
+        boolean condicao2= true;
+
+        if (condicao1 && ( 7 > 4)) {
+            System.out.println("as duas condicoes sao verdadeiras");
+        }
+
+        if (condicao1 || condicao2) {
+            System.out.println("Uma das condicoes é verdadeira");
+        }
+
+        System.out.println("fim");
+
+    }
+}
--- tipo-variaveis/src/TiposVariaveis.java
+++ tipo-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,14 @@
-public class App {
+public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //tipos primitivos
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        final double VALOR_DE_PI = 3.14;
+
+        System.out.println(VALOR_DE_PI);
     }
 }
